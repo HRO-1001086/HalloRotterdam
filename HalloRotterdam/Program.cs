3204b65b6a2741825020285ba0d58933ee92e3b5
Kais <kais.ahmad2000@gmail.com>
2021-03-22 14:17:03 +0100

    Groetjes

--- HalloRotterdam/Program.cs
+++ HalloRotterdam/Program.cs
@@ -7,7 +7,7 @@ namespace HalloRotterdam
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo Rotterdam!");
-            Console.WriteLine("Groeten van Kais!");
+            Console.WriteLine("Groetjes van Kais!");
         }
     }
 }
